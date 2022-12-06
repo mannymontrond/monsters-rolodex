aea4ed3cb3f35aa1e5274b9a1ac264aa17cca644
Manuel Montrond <mmontrond@granitenet.com>
2022-12-06 14:51:36 -0500

    adding test

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 /*Test file
 
+git status
+git  add . ( . for all or specific files)
+git commit: -> local commit
+git push -> pushed commited changed to the repo
+get clone to initally create file
 end of test*/
