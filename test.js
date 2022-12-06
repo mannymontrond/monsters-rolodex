3bca6b197e97da6f3c5547d36a335b42265bc68a
Manuel Montrond <mmontrond@granitenet.com>
2022-12-06 15:27:56 -0500

    test

--- test.js
+++ test.js
@@ -5,4 +5,10 @@ git  add . ( . for all or specific files)
 git commit: -> local commit
 git push -> pushed commited changed to the repo
 get clone to initally create file
+
+git pull -> pull changes from the server
+branching and merging -----
+git branch stage
+git checkeout state -> swith to stage branch
+
 end of test*/
